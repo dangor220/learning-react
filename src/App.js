81997d1ea392a39fdd89e55696b20e067f62210c
Danil Gordeev <dan.gor220@yandex.ru>
2022-11-14 19:21:11 +0300

    feat: add select form

--- src/App.js
+++ src/App.js
@@ -294,32 +294,74 @@ import './App.css';
 // }
 
 
+// export default class App extends Component {
+// 	render() {
+// 		return <NameForm />;
+// 	}
+// }
+
+// class NameForm extends Component {
+// 	constructor(props) {
+// 		super(props);
+// 		this.state = {value: ''};
+// 		this.handleChange = this.handleChange.bind(this);
+// 		this.handleSubmit = this.handleSubmit.bind(this);
+// 	}
+// 	handleChange(event) {
+// 		this.setState({value: event.target.value}) 
+// 	}
+// 	handleSubmit(event) {
+// 		alert('Name user ' + this.state.value);
+// 		event.preventDefault();
+// 	}
+
+// 	render() {
+// 		return (
+// 			<form onSubmit={this.handleSubmit}>
+// 				Name:
+// 				<input type="text" value={this.state.value} onChange={this.handleChange} />
+// 				<input type="submit" value="submit"/>
+// 			</form>
+// 		)
+// 	}
+// }
+
 export default class App extends Component {
 	render() {
-		return <NameForm />;
+		return <SelectForm />;
 	}
 }
 
-class NameForm extends Component {
+
+class SelectForm extends Component {
 	constructor(props) {
 		super(props);
-		this.state = {value: ''};
+		this.state = {value: 'coconut'};
+
 		this.handleChange = this.handleChange.bind(this);
 		this.handleSubmit = this.handleSubmit.bind(this);
 	}
+
 	handleChange(event) {
-		this.setState({value: event.target.value}) 
+		this.setState({value: event.target.value})
 	}
 	handleSubmit(event) {
-		alert('Name user ' + this.state.value);
-		event.preventDefault();
+		alert('Selec default: ' + this.state.value);
+		event.preventDefault()
 	}
 
 	render() {
 		return (
 			<form onSubmit={this.handleSubmit}>
-				Name:
-				<input type="text" value={this.state.value} onChange={this.handleChange} />
+			<label>
+				Select product:
+				<select value={this.state.value} onChange={this.handleChange}>
+					<option value="grapefruit">Грейпфрут</option>
+					<option value="lime">Лайм</option>
+					<option value="coconut">Кокос</option>
+					<option value="mango">Манго</option>
+				</select>
+			</label>
 				<input type="submit" value="submit"/>
 			</form>
 		)

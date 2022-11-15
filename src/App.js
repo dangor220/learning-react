447c648a453655c0e04abd11305a196ca489af19
Danil Gordeev <dan.gor220@yandex.ru>
2022-11-16 00:35:50 +0300

    add: sync between celsius and farengate

--- src/App.js
+++ src/App.js
@@ -431,31 +431,87 @@ function BoilinVerdict(props) {
 	return <p>Вода не закипит!</p>;
 }
 
+const scaleNames = {
+	c: 'Цельсия',
+	f: 'Фаренгейта',
+};
 class Calculator extends Component {
 	constructor(props) {
 		super(props);
-		this.state = { temperature: '' };
-		this.handleChange = this.handleChange.bind(this);
+		this.state = { temperature: '', scale: 'c' };
+		this.handleCelsiusChange = this.handleCelsiusChange.bind(this);
+		this.handleFahrenheitChange = this.handleFahrenheitChange.bind(this);
 	}
 
-	handleChange(e) {
-		this.setState({ temperature: e.target.value });
+	handleCelsiusChange(temperature) {
+		this.setState({ scale: 'c', temperature });
+	}
+	handleFahrenheitChange(temperature) {
+		this.setState({ scale: 'f', temperature });
 	}
 
 	render() {
+		const scale = this.state.scale;
+		const temperature = this.state.temperature;
+		const celsius =
+			scale === 'f' ? tryConvert(temperature, toCelsius) : temperature;
+		const fahrenheit =
+			scale === 'c' ? tryConvert(temperature, toFahrenheit) : temperature;
 		return (
 			<div>
-				<label>
-					Цельсий:` `
-					<input
-						onChange={this.handleChange}
-						type="number"
-						placeholder={'Введите температуру'}
-					/>
-				</label>
-
-				<BoilinVerdict celsius={this.state.temperature} />
+				<TemperatureInput
+					scale="c"
+					onTemperatureChange={this.handleCelsiusChange}
+					temperature={celsius}
+				/>
+				<TemperatureInput
+					scale="f"
+					onTemperatureChange={this.handleFahrenheitChange}
+					temperature={fahrenheit}
+				/>
+				<BoilinVerdict celsius={parseFloat(celsius)} />
 			</div>
 		);
 	}
+}
+
+class TemperatureInput extends Component {
+	constructor(props) {
+		super(props);
+		this.handleChange = this.handleChange.bind(this);
+		// this.state = { temperature: '' };
+	}
+	handleChange(e) {
+		// this.setState({ temperature: e.target.value });
+		this.props.onTemperatureChange(e.target.value);
+	}
+
+	render() {
+		const temperature = this.props.temperature;
+		const scale = this.props.scale;
+
+		return (
+			<fieldset>
+				<legend>Введите температуру в градусах по {scaleNames[scale]}</legend>
+				<input onChange={this.handleChange} type="number" value={temperature} />
+			</fieldset>
+		);
+	}
+}
+
+function toCelsius(fahrenheit) {
+	return ((fahrenheit - 32) * 5) / 9;
+}
+function toFahrenheit(celsius) {
+	return (celsius * 9) / 5 + 32;
+}
+
+function tryConvert(temperature, convert) {
+	const input = parseFloat(temperature);
+	if (Number.isNaN(input)) {
+		return '';
+	}
+	const output = convert(input);
+	const rounded = Math.round(output * 1000) / 1000;
+	return rounded.toString();
 }
